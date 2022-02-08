90f460a62fcb092ba5f3fa754fce4f2ca3f9b076
Loacir-Zen <loacir.zb1990@gmail.com>
2022-02-07 23:10:23 -0300

    Introdução a JavaScript

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+console.log("Trabalhando com variáveis");
+const idade = 29;
+const Idade = 32;
+
+console.log(idade);
+console.log(Idade);
+
+//não fazer
+//mes = "janeiro";
---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 29;
+//let nome = "Loacir"; não é uma boa pratica
+const nome = "Loacir";
+const sobrenome = "Brito";
+
+console.log(nome, sobrenome);
+console.log(nome + " " + sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+
+//não fazer
+//mes = "janeiro";
