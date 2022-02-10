df3480fbfa3f080fdb5fcba235ef6daddcf5ab77
Loacir-Zen <loacir.zb1990@gmail.com>
2022-02-10 17:09:51 -0300

    Concluído módulo 2 - JavaScrip Orientado a Objeto

--- Parte-2/ContaCorrente.js
+++ Parte-2/ContaCorrente.js
@@ -1,8 +1,38 @@
-export class ContaCorrente {
-  agencia;
-  cliente;
+import { Cliente } from "./Cliente.js";
 
+export class ContaCorrente {
+  //static para refletir em todas as instância
+  //ou seja sempre que for chamado o metodo construtor
+  //independente do objetor for diferente
+  //mesmo assim ocorre um incremento
+  static numeroDeContas = 0;
+  agencia;
+  _cliente;
   _saldo = 0;
+
+  set cliente(novoValor) {
+    if (novoValor instanceof Cliente) {
+      this._cliente = novoValor;
+    }
+  }
+
+  get cliente() {
+    return this._cliente;
+  }
+
+  get saldo() {
+    return this._saldo;
+  }
+
+  constructor(cliente, agencia) {
+    this.agencia = agencia;
+    //nesse caso estamos pegando o acessor get cliente() e não atributo _cliente
+    this.cliente = cliente;
+    //aqui está usando a classe ContaCorrente e não o this
+    //porque estamos usando um atributo static
+    //que deve ser refletido em todas as instâncias
+    ContaCorrente.numeroDeContas += 1;
+  }
 
   sacar(valor) {
     if (this._saldo >= valor) {
--- Parte-2/index.js
+++ Parte-2/index.js
@@ -1,23 +1,25 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-const cliente3 = new Cliente();
+const cliente1 = new Cliente("Loacir", 11122233344);
+const cliente2 = new Cliente("João", 22211133344);
+const cliente3 = new Cliente("Maria", 33311122244);
 
-cliente1.nome = "Loacir";
-cliente1.cpf = 11122233344;
+//
+//cliente1.nome = "Loacir";
+//cliente1.cpf = 11122233344;
 
-cliente2.nome = "João";
-cliente2.cpf = 22211133344;
+//cliente2.nome = "João";
+//cliente2.cpf = 22211133344;
 
-cliente3.nome = "Maria";
-cliente3.cpf = 33311122244;
+//cliente3.nome = "Maria";
+//cliente3.cpf = 33311122244;
 
-const contaCorrenteLoacir = new ContaCorrente();
-contaCorrenteLoacir.agencia = 1001;
+const contaCorrenteLoacir = new ContaCorrente(cliente1, 1001);
 
-contaCorrenteLoacir.cliente = cliente1;
+//não precisa mais pois criamos os contrutores
+//contaCorrenteLoacir.agencia = 1001;
+//contaCorrenteLoacir.cliente = cliente1;
 
 console.log(contaCorrenteLoacir);
 contaCorrenteLoacir.depositar(120);
@@ -28,15 +30,21 @@ console.log(contaCorrenteLoacir);
 contaCorrenteLoacir.depositar(500);
 console.log(contaCorrenteLoacir);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 102;
-console.log(conta2);
+const conta2 = new ContaCorrente(cliente2, 102);
+//acesso para proteção (get e set ele não é um método ele é uma acessor)
+//o acessor sempre vai ser público
+//conta2.cliente = 0; vai retornar undefined porque set cliente(novoValor) temos um instanceof para Cliente
+//o que obriga nos passarmos um objeto como deveria ser
+//isso obriga que quando atribuimos um valor para uma um objeto ele dever do tipo cliente
 
-//console.log(cliente1);
-//console.log(cliente2);
-//console.log(cliente3);
+//conta2.agencia = 102;
+//conta2.cliente = cliente2;
 
-contaCorrenteLoacir.transferir(200, conta2);
-console.log(conta2);
+let valor = 200;
+contaCorrenteLoacir.transferir(valor, conta2);
+console.log(conta2.cliente);
 console.log(contaCorrenteLoacir);
+
+//console.log(conta2.numeroDeContas);
+//para acessar o atributo static da contaCorrente
+console.log(ContaCorrente.numeroDeContas);
